f40baf297da00495f465b3c64fdbee40bf2b61ab
GreenStoreTW <littletiger0815@gmail.com>
2026-09-18 20:32:44 +0800

    fix: add importmap for firebase CDN to index.html and bump SW to v35

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // FlatSpec Drive - Service Worker (Offline First Support)
-const CACHE_NAME = 'flatspec-cache-v34';
+const CACHE_NAME = 'flatspec-cache-v35';
 const PRECACHE_ASSETS = [
   './',
   './index.html',
